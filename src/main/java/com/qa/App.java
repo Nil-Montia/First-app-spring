49d2dc0daa3f1884625d3efedc9f38eba23d0ced
Nil Montia <nil.montia@academytrainee.com>
2019-04-17 11:07:31 +0100

    added functionality

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -3,12 +3,13 @@ package com.qa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class App
 {
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
-
     }
 }
