a12e624e3e607c21e3ab171c062a9af73fe7f18d
Nil Montia <nil.montia@academytrainee.com>
2019-05-15 15:21:14 +0100

    added jar packaging

--- src/main/java/com/qa/Controller/BlockController.java
+++ src/main/java/com/qa/Controller/BlockController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/block/")
+@RequestMapping("api/block/")
 public class BlockController {
 
     @Autowired
